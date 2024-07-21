475d9d293b0f14861e795bc90ae0e6e6ba1457f9
SaurabhJais26 <sjsaurabhjais@gmail.com>
2024-07-21 20:47:00 +0530

    Initial project setup and configuration.

---
+++ Thoughts/Models/BlogPost.swift
@@ -0,0 +1,17 @@
+//
+//  BlogPost.swift
+//  Thoughts
+//
+//  Created by Saurabh Jaiswal on 21/07/24.
+//
+
+import Foundation
+
+
+struct BlogPost {
+    let identifier: String
+    let title: String
+    let timestamp: TimeInterval
+    let headerImageUrl: URL?
+    let text: String
+}
---
+++ Thoughts/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Thoughts
+//
+//  Created by Saurabh Jaiswal on 21/07/24.
+//
+
+import Foundation
+
+
+struct User {
+    let name: String
+    let email: String
+    let profilePictureUrl: URL?
+}
