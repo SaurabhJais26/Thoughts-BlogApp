34388fd6f94d412958bd4ea17f0ea74f4fc8475e
SaurabhJais26 <sjsaurabhjais@gmail.com>
2024-07-23 14:13:21 +0530

    Added sign in and sign up configuration also insetion in db.

--- Thoughts/Controllers/Core Tabs/ProfileViewController.swift
+++ Thoughts/Controllers/Core Tabs/ProfileViewController.swift
@@ -16,6 +16,27 @@ class ProfileViewController: UIViewController {
     }
     
     @objc private func didTapSignOut() {
-        
+        let alert = UIAlertController(title: "Sign Out", message: "Are you sure you'd like to sign out?", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Sign Out", style: .destructive, handler: { _ in
+            AuthManager.shared.signOut { [weak self] success in
+                if success {
+                    DispatchQueue.main.async {
+                        UserDefaults.standard.set(nil, forKey: "email")
+                        UserDefaults.standard.set(nil, forKey: "name")
+                        
+                        let signInVC = SignInViewController()
+                        signInVC.navigationItem.largeTitleDisplayMode = .always
+                        
+                        let navVC = UINavigationController(rootViewController: signInVC)
+                        navVC.navigationBar.prefersLargeTitles = true
+                        
+                        navVC.modalPresentationStyle = .fullScreen
+                        self?.present(navVC, animated: true, completion: nil)
+                    }
+                }
+            }
+        }))
+        present(alert, animated: true)
     }
 }
